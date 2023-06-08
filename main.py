e80036197fed3d0cfd928260255a70c526c5b53e
lbrandon13 <31885900+lbrandon13@users.noreply.github.com>
2023-06-08 09:14:16 +0100

    Add folder for files, put folder path as variable

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
 from openpyxl import load_workbook
 
-workbook = load_workbook("D:\Work\Coding\battleboard_data_extraction\Wulfric_baneguard_current_v_2021.1.xlsm")
+source_folder = "d:\Work\Coding\\battleboard_data_extraction\\battleboard-data-extraction\source_files\\"
 
-print(workbook.sheetnames)
+test_workbook = load_workbook(source_folder + "Wulfric_baneguard_current_v_2021.1.xlsm")
+
+print(test_workbook.sheetnames)
 
