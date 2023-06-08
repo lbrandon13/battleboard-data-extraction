19c906514af339db67b6116af46f07b1abeffc3e
lbrandon13 <31885900+lbrandon13@users.noreply.github.com>
2023-06-08 12:42:41 +0100

    Add list of the text for each skill in order

--- main.py
+++ main.py
@@ -3,9 +3,19 @@
 
 from openpyxl import load_workbook
 
+from textLists import skillList
+
 source_folder = "d:\Work\Coding\\battleboard_data_extraction\\battleboard-data-extraction\source_files\\"
 
 test_workbook = load_workbook(source_folder + "Wulfric_baneguard_current_v_2021.1.xlsm")
 
 print(test_workbook.sheetnames)
+
+sheet = test_workbook['The Character']
+
+# # openpyxl starts at 1 for row and column
+# for i in range(2,sheet.max_row):
+
+print(len(skillList))
+
 
