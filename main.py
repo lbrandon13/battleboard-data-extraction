2f3aa88e662fb9d3aa019dc8591acecc1b435c5a
lbrandon13 <31885900+lbrandon13@users.noreply.github.com>
2023-06-08 16:13:43 +0100

    Grab skills from multiple files

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
 from openpyxl import load_workbook
@@ -7,27 +6,49 @@ from textLists import skillList
 
 source_folder = "d:\Work\Coding\\battleboard_data_extraction\\battleboard-data-extraction\source_files\\"
 
-test_workbook = load_workbook(source_folder + "Wulfric_baneguard_current_v_2021.1.xlsm")
+fileList = ["Wulfric_baneguard_current_v_2021.1.xlsm",
+            "emilie_oct_23.xlsm",
+            ]
 
-print(test_workbook.sheetnames)
+# test_workbook = load_workbook(source_folder + "Wulfric_baneguard_current_v_2021.1.xlsm")
 
-sheet = test_workbook['The Character']
+# print(test_workbook.sheetnames)
+# ['Instructions', 'Adventure Record', 'The Character', 'Magic', 'Power', 'Armour', 'Battleboard', 'Notes', 'Base', 'Variables', 'Tables', 'STD HQA']
+
+# skill name maps to dict with 2 keys, 1 to track total ranks, 1 to track number of files that formed the total
+# should look like
+# skillMap = {'Bonebreak' : {'ranks' : 4,
+#                            'characters' : 2},
+#             'dodge'     : {'ranks' : 1,
+#                            'characters' : 1}
+#             } 
 
 skillMap = {}
 
-# openpyxl starts at 1 for row and column
-for i in range(14,397):
-    
-    if sheet.cell(row=i,column=3).value != None:
+for file in fileList:
+
+    currentWorkbook = load_workbook(source_folder + file)
+    skillSheet = currentWorkbook['The Character']
+
+    # openpyxl starts at 1 for row and column
+    for i in range(14,397):
+        
         # allow for offset between row number on sheet and position in list
-        try:
-            skillMap[skillList[(i-14)]] = sheet.cell(row=i,column=3).value
-        except Exception as e:
-            print("Incorrect Number of skills, likely wrong battleboard version")
-            raise e
+        skillName = skillList[(i-14)]
+        rankValue = skillSheet.cell(row=i,column=3).value 
+
+        if rankValue != None:
+
+            if skillName not in skillMap:
+                skillMap[skillName] = {'ranks' : rankValue,
+                                       'characters' : 1}
+            else:
+                skillMap[skillName]['ranks'] = skillMap[skillName]['ranks'] + rankValue
+                skillMap[skillName]['characters'] = skillMap[skillName]['characters'] + 1 
+                  
 
 # print(sheet.cell(row=15,column=3).value)
 
 # print(len(skillList))
-print(skillMap)
+# print(skillMap)
 
